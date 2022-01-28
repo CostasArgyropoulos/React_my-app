faccd7b649ec9dd2faebc51821ba38bd6003e9e9
cargyrop <cargyrop@frtntech.com>
2022-01-28 16:30:20 +0200

    First stage of our application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React from "react";
+import Expenses from "./components/Expenses";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2022, 1, 28),
+    },
+    {
+      id: "e2",
+      title: "Toiler Paper",
+      amount: 94.12,
+      date: new Date(2021, 7, 14),
+    },
+    { id: "e3", title: "New TV", amount: 799.99, date: new Date(2021, 2, 12) },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2020, 5, 12),
+    },
+  ];
+
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Lets get started"),
+    React.createElement(Expenses, {items: expenses})
   );
+
+  // return (
+  //   <div>
+  //     <Expenses items={expenses} />
+  //   </div>
+  // );
 }
 
 export default App;
