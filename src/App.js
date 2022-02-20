74af5687c9feb3d94aa56e41b29809ba4ae7f7f3
cargyrop <cargyrop@frtntech.com>
2022-02-20 17:26:57 +0200

    Controlled component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
